f3f36b6cc85ca11ae9eabc753b3ad894b9727167
CallistaEve <70805652+CallistaEve@users.noreply.github.com>
2023-04-20 07:18:25 +0700

    Initial Commit

---
+++ 0706012110036-Evelyn-Callista-Yaurentius-AFL3-RPG_BATTLE/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  0706012110036-Evelyn-Callista-Yaurentius-AFL3-RPG_BATTLE
+//
+//  Created by MacBook Pro on 20/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 0706012110036-Evelyn-Callista-Yaurentius-AFL3-RPG_BATTLE/_706012110036_Evelyn_Callista_Yaurentius_AFL3_RPG_BATTLEApp.swift
@@ -0,0 +1,17 @@
+//
+//  _706012110036_Evelyn_Callista_Yaurentius_AFL3_RPG_BATTLEApp.swift
+//  0706012110036-Evelyn-Callista-Yaurentius-AFL3-RPG_BATTLE
+//
+//  Created by MacBook Pro on 20/04/23.
+//
+
+import SwiftUI
+
+@main
+struct _706012110036_Evelyn_Callista_Yaurentius_AFL3_RPG_BATTLEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
